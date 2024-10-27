34bed9fde9cee667b57ce4fc055658017a9d58e9
parsarezaie78 <35139045+parsarezaie78@users.noreply.github.com>
2024-10-27 20:03:43 +0330

    Update helper.js

--- app/src/utils/helper.js
+++ app/src/utils/helper.js
@@ -1,8 +1,10 @@
-static checkReferralCode(referralCode) {
-    const myReferralCode = 'TMCDAV'; // کد رفرال جدید شما
+class Helper {
+    static checkReferralCode(referralCode) {
+        const myReferralCode = 'TMCDAV'; // کد رفرال شما
 
-    if (referralCode !== myReferralCode) {
-        throw new Error('Sorry, You cannot use this bot, please join with creator referral code');
+        if (referralCode !== myReferralCode) {
+            throw new Error('Sorry, You cannot use this bot, please join with creator referral code');
+        }
+        return true;
     }
-    return true;
 }
